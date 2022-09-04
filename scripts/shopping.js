c8b0fee4a376081dff741215aa3b839554c32e9b
lemstra <sebastian.lemstra@disy.net>
2022-09-04 17:15:39 +0200

    Small Update

--- scripts/shopping.js
+++ scripts/shopping.js
@@ -18,40 +18,42 @@ var holdStop = null;
 //Kategorie, Item, CustomText, Status, RankRecent, DatumUpdate
 var items = [
     ["Obst/Gemüse", "Banane", "1", "other", "", ""],
-    ["Obst/Gemüse", "Kiwi", "2", "other", "", ""],
-    ["Obst/Gemüse", "Gurke", "3", "open", "", ""],
-    ["Obst/Gemüse", "Tomate", "4", "open", "", ""],
-    ["Obst/Gemüse", "Aubergine", "5", "other", "", ""],
+    ["Obst/Gemüse", "Kiwi", "", "other", "", ""],
+    ["Obst/Gemüse", "Gurke", "", "open", "", ""],
+    ["Obst/Gemüse", "Tomate", "200g", "open", "", ""],
+    ["Obst/Gemüse", "Aubergine", "", "other", "", ""],
     ["Backwaren", "Brot", "6", "other", "", ""],
-    ["Backwaren", "Kekse", "7", "open", "", ""],
-    ["Backwaren", "Brötchen", "8", "other", "", ""],
-    ["Molkereiprodukte", "Käse", "9", "other", "", ""],
-    ["Molkereiprodukte", "Frischkäse", "0", "recent", "0", ""],
-    ["Molkereiprodukte", "Frischmilch", "ß", "other", "", ""],
+    ["Backwaren", "Kekse", "", "open", "", ""],
+    ["Backwaren", "Brötchen", "", "other", "", ""],
+    ["Molkereiprodukte", "Käse", "", "other", "", ""],
+    ["Molkereiprodukte", "Frischkäse", "", "recent", "0", ""],
+    ["Molkereiprodukte", "Frischmilch", "", "other", "", ""],
     ["Molkereiprodukte", "Butter", "11", "other", "", ""]
-]
+];
+
+const categories = ["Obst/Gemüse", "Backwaren", "Molkereiprodukte", "Fleisch/Fisch", "Zutaten/Gewürze", "Fertig-/Tiefkühlprodukte", "Süßwaren", "Haushalt", "Baumarkt/Garten", "Eigene Items"];
 
 //functions
-function moveItem(e){
+function moveItem(e) {
     const clickedItem = e.target.firstChild.innerText;
     const clickedItemIndex = getIndexOfItem(clickedItem);
     const clickedItemStatus = items[clickedItemIndex][3];
     getHtmlElement(clickedItem);
     items[clickedItemIndex][5] = new Date();
-    if (clickedItemStatus === "open"){
+    if (clickedItemStatus === "open") {
         updateRecentItems();
         items[clickedItemIndex][3] = "recent";
         items[clickedItemIndex][4] = "0";
         e.target.remove();
         addItemToDOM(clickedItem, items[clickedItemIndex][2], recentItems);
-    } else if (clickedItemStatus === "recent"){
+    } else if (clickedItemStatus === "recent") {
         rank = items[clickedItemIndex][4];
         items[clickedItemIndex][4] = "";
         items[clickedItemIndex][3] = "open";
         decreaseRecentRank(rank);
         e.target.remove();
         addItemToDOM(clickedItem, items[clickedItemIndex][2], openItems);
-    } else if (clickedItemStatus === "other"){
+    } else if (clickedItemStatus === "other") {
         items[clickedItemIndex][3] = "open";
         e.target.remove();
         addItemToDOM(clickedItem, items[clickedItemIndex][2], openItems);
@@ -60,20 +62,20 @@ function moveItem(e){
     localStorage.setItem("localItemStorage", JSON.stringify(items));
 }
 
-function decreaseRecentRank(rank){
-    items.forEach(function(value, index, array) {
-        if(value[3] === "recent"){
-            if (parseInt(value[4]) > rank){
+function decreaseRecentRank(rank) {
+    items.forEach(function (value, index, array) {
+        if (value[3] === "recent") {
+            if (parseInt(value[4]) > rank) {
                 items[index][4] = parseInt(items[index][4]) - 1;
-            } 
+            }
         }
     })
 }
 
-function updateRecentItems(){
-    items.forEach(function(value, index, array) {
-        if(value[3] === "recent"){
-            if (parseInt(value[4]) > 4){
+function updateRecentItems() {
+    items.forEach(function (value, index, array) {
+        if (value[3] === "recent") {
+            if (parseInt(value[4]) > 4) {
                 items[index][4] = "";
                 items[index][3] = "other";
                 getHtmlElement(value[1]).remove();
@@ -85,28 +87,28 @@ function updateRecentItems(){
     })
 }
 
-function getHtmlElement(item){
+function getHtmlElement(item) {
     for (const element of document.querySelectorAll('.item')) {
         if (element.innerText.includes(item)) {
-            return(element);
+            return (element);
         }
     }
 }
 
-function getIndexOfItem(item){
+function getIndexOfItem(item) {
     let itemIndex = -1;
-    items.forEach(function(value, index, array) {
-       if(value[1] === item){
-        itemIndex = index
-       }
+    items.forEach(function (value, index, array) {
+        if (value[1] === item) {
+            itemIndex = index
+        }
     })
-    return(itemIndex);
+    return (itemIndex);
 }
 
-function addToOpenItems(e){
+function addToOpenItems(e) {
     //Prevent natural behaviour
     e.preventDefault();
-    if (itemInput.value != ""){
+    if (itemInput.value != "") {
         addItemToDOM(itemIntput.value, openItems);
         itemIntput.value = "";
     } else {
@@ -114,7 +116,7 @@ function addToOpenItems(e){
     }
 }
 
-function addItemToDOM(item, description, targetElement){
+function addItemToDOM(item, description, targetElement) {
     const itemDiv = document.createElement("button");
     itemDiv.classList.add("item");
 
@@ -130,25 +132,25 @@ function addItemToDOM(item, description, targetElement){
     additionalInfo.innerText = description;
     additionalInfo.classList.add("additionalInfo");
     itemDiv.appendChild(additionalInfo);
-    
+
     targetElement.appendChild(itemDiv);
 }
 
-function onMouseDown(){
+function onMouseDown() {
     holdStart = new Date();
 }
 
-function onMouseUp(e){
+function onMouseUp(e) {
     holdStop = new Date();
     let difference = holdStop - holdStart;
-    if (difference > 350){
+    if (difference > 350) {
         addDescription(e);
     } else {
         moveItem(e);
     }
 }
 
-function addDescription(e){
+function addDescription(e) {
     const clickedItem = e.target.firstChild.innerText;
     const clickedItemIndex = getIndexOfItem(clickedItem);
     const clickedItemStatus = items[clickedItemIndex][3];
@@ -157,44 +159,68 @@ function addDescription(e){
         overlay.removeChild(overlay.firstChild);
     }
 
+    const buttonCloseAdditionalInfoInput = document.createElement("button");
+    buttonCloseAdditionalInfoInput.innerHTML = "<i class='fa-sharp fa-solid fa-circle-xmark'></i>";
+    buttonCloseAdditionalInfoInput.setAttribute("id", "buttonCloseAdditionalInfoInput");
+    buttonCloseAdditionalInfoInput.addEventListener("click", closeAdditionalInfo);
+
     const titleSection = document.createElement("section");
+    titleSection.classList.add("titleSection")
     const title = document.createElement("h1");
     title.setAttribute("id", "title");
     title.innerHTML = clickedItem;
     titleSection.appendChild(title);
-    overlay.appendChild(titleSection); 
-    
+    titleSection.appendChild(buttonCloseAdditionalInfoInput);
+
+    overlay.appendChild(titleSection);
+
     const additionalInfoInput = document.createElement("input");
     additionalInfoInput.type = "text";
-    additionalInfoInput.value = items[clickedItemIndex][2]
+    let inputText = items[clickedItemIndex][2];
+    if (inputText === "") {
+        inputText = "Menge, Beschreibung...";
+    }
+    additionalInfoInput.value = inputText;
     additionalInfoInput.setAttribute("id", "additionalInfoInput");
-
-    const buttonCloseAdditionalInfoInput = document.createElement("button");
-    buttonCloseAdditionalInfoInput.innerText = "Close";
-    buttonCloseAdditionalInfoInput.setAttribute("id", "buttonCloseAdditionalInfoInput");
-    buttonCloseAdditionalInfoInput.addEventListener("click", closeAdditionalInfo);
-
     overlay.appendChild(additionalInfoInput);
-    overlay.appendChild(buttonCloseAdditionalInfoInput);
+
+    const selectCategory = document.createElement("select");
+    selectCategory.classList.add("dropDown");
+    for(let i=0; i<categories.length; i++){
+        const newOption = document.createElement("option");
+        newOption.textContent = categories[i];
+        newOption.value = categories[i];
+        selectCategory.appendChild(newOption);
+    }
+    selectCategory.value = items[clickedItemIndex][0];
+    overlay.appendChild(selectCategory);
 }
 
-function closeAdditionalInfo(){
+function closeAdditionalInfo() {
     let updatedItemIndex = getIndexOfItem(document.getElementById("title").innerHTML);
-    items[updatedItemIndex][2] = document.getElementById("additionalInfoInput").value;
+    let additionalInputText = document.getElementById("additionalInfoInput").value;
+    console.log(additionalInputText);
+    if (additionalInputText === "Menge, Beschreibung...") {
+        additionalInputText = ""
+    }
+    items[updatedItemIndex][2] = additionalInputText;
     //update local storage
     localStorage.setItem("localItemStorage", JSON.stringify(items));
 
-    document.getElementById("buttonCloseAdditionalInfoInput").remove();
+    /*document.getElementById("buttonCloseAdditionalInfoInput").remove();
     document.getElementById("additionalInfoInput").remove();
-    document.getElementById("title").remove();
+    document.getElementById("title").remove();*/
+    while (overlay.firstChild) {
+        overlay.removeChild(overlay.firstChild);
+    }
     createListView();
 }
 
-function createListView(){
+function createListView() {
     const titleSection = document.createElement("section");
     titleSection.innerHTML = "<h1 id='title'>Einkaufsliste</h1>";
     overlay.appendChild(titleSection);
-    
+
     const controls = document.createElement("div");
     controls.setAttribute("id", "controls");
     controls.innerHTML = "<input type='text' id='itemInput'></input>    <button id='addItemButton' type='submit'>      <i class='fas fa-plus-square'></i>    </button>"
@@ -235,25 +261,25 @@ function createListView(){
     addItemButton.addEventListener("click", addToOpenItems);
 
     updateFromLocalStroage();
-  
+
     //add all Items to UI
     items.forEach(element => {
-        if (element[3] === "open"){
+        if (element[3] === "open") {
             addItemToDOM(element[1], element[2], openItems);
         }
-        if (element[3] === "recent"){
+        if (element[3] === "recent") {
             addItemToDOM(element[1], element[2], recentItems);
         }
-        if (element[3] === "other"){
+        if (element[3] === "other") {
             addItemToDOM(element[1], "", otherItems);
         }
     });
 }
 
-function updateFromLocalStroage(){
+function updateFromLocalStroage() {
     if (localStorage.getItem("localItemStorage") === null) {
         localStorage.setItem("localItemStorage", JSON.stringify(items));
     } else {
         items = JSON.parse(localStorage.getItem("localItemStorage"));
-    } 
+    }
 }
