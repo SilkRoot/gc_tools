5c245efe739592493b7e1cf0a1ede807d7535a72
lemstra <sebastian.lemstra@disy.net>
2022-10-15 23:44:49 +0200

    reorganize code

--- backend/main.py
+++ backend/main.py
@@ -3,41 +3,30 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-#countries = [
-#    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-#    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-#    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408}
-#]
-
-#itemList = [
-#    {"id": 1,  "category": "Obst/Gemüse",        "type": "Banane",       "custom": "1",      "status": "other", "rank": "",  "update": ""},
-#    {"id": 2,  "category": "Obst/Gemüse",        "type": "Kiwi",         "custom": "",       "status": "other", "rank": "",  "update": ""},
-#    {"id": 3,  "category": "Obst/Gemüse",        "type": "Gurke",        "custom": "",       "status": "open",  "rank": "",  "update": ""},
-#    {"id": 4,  "category": "Obst/Gemüse",        "type": "Tomate",       "custom": "200g",   "status": "open",  "rank": "",  "update": ""},
-#    {"id": 5,  "category": "Obst/Gemüse",        "type": "Aubergine",    "custom": "",       "status": "other", "rank": "",  "update": ""},
-#    {"id": 6,  "category": "Backwaren",          "type": "Brot",         "custom": "6",      "status": "other", "rank": "",  "update": ""},
-#    {"id": 7,  "category": "Backwaren",          "type": "Kekse",        "custom": "",       "status": "open",  "rank": "",  "update": ""},
-#    {"id": 8,  "category": "Backwaren",          "type": "Brötchen",     "custom": "",       "status": "other", "rank": "",  "update": ""},
-#    {"id": 9,  "category": "Molkereiprodukte",   "type": "Käse",         "custom": "",       "status": "other", "rank": "",  "update": ""},
-#    {"id": 10, "category": "Molkereiprodukte",   "type": "Frischkäse",   "custom": "",       "status": "other", "rank": "0", "update": ""},
-#    {"id": 11, "category": "Molkereiprodukte",   "type": "Frischmilch",  "custom": "",       "status": "other", "rank": "",  "update": ""},
-#    {"id": 0,  "category": "Molkereiprodukte",   "type": "Butter",       "custom": "11",     "status": "other", "rank": "",  "update": ""}
-#]
-
-
 itemList = [
-    {"id": 1,  "category": "Obst/Gemüse",        "type": "Banane",       "custom": "1",      "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 2,  "category": "Obst/Gemüse",        "type": "Kiwi",         "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 3,  "category": "Obst/Gemüse",        "type": "Gurke",        "custom": "",       "status": "open",  "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 4,  "category": "Obst/Gemüse",        "type": "Tomate",       "custom": "200g",   "status": "open",  "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 5,  "category": "Obst/Gemüse",        "type": "Aubergine",    "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 6,  "category": "Backwaren",          "type": "Brot",         "custom": "6",      "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 7,  "category": "Backwaren",          "type": "Kekse",        "custom": "",       "status": "open",  "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 8,  "category": "Backwaren",          "type": "Brötchen",     "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 9,  "category": "Molkereiprodukte",   "type": "Käse",         "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 10, "category": "Molkereiprodukte",   "type": "Frischkäse",   "custom": "",       "status": "other", "rank": "0", "update": "0000000000000", "changed": 0},
-    {"id": 11, "category": "Molkereiprodukte",   "type": "Frischmilch",  "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
-    {"id": 0,  "category": "Molkereiprodukte",   "type": "Butter",       "custom": "11",     "status": "other", "rank": "",  "update": "0000000000000", "changed": 0}
+    {"id": 1,   "category": "Obst/Gemüse",              "type": "Banane",       "custom": "1",      "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 2,   "category": "Obst/Gemüse",              "type": "Kiwi",         "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 3,   "category": "Obst/Gemüse",              "type": "Gurke",        "custom": "",       "status": "open",  "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 4,   "category": "Obst/Gemüse",              "type": "Tomate",       "custom": "200g",   "status": "open",  "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 5,   "category": "Obst/Gemüse",              "type": "Aubergine",    "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 6,   "category": "Backwaren",                "type": "Brot",         "custom": "6",      "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 7,   "category": "Backwaren",                "type": "Kekse",        "custom": "",       "status": "open",  "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 8,   "category": "Backwaren",                "type": "Brötchen",     "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 9,   "category": "Molkereiprodukte",         "type": "Käse",         "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 10,  "category": "Molkereiprodukte",         "type": "Frischkäse",   "custom": "",       "status": "recent", "rank": "0", "update": "0000000000000", "changed": 0},
+    {"id": 11,  "category": "Molkereiprodukte",         "type": "Frischmilch",  "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": 0,   "category": "Molkereiprodukte",         "type": "Butter",       "custom": "11",     "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -1,  "category": "Obst/Gemüse",              "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -2,  "category": "Backwaren",                "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -3,  "category": "Molkereiprodukte",         "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -4,  "category": "Fleisch/Fisch",            "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -5,  "category": "Zutaten/Gewürze",          "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -6,  "category": "Fertig-/Tiefkühlprodukte", "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -7,  "category": "Süßwaren",                 "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -8,  "category": "Haushalt",                 "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -9,  "category": "Baumarkt/Garten",          "type": "dummy",        "custom": "",       "status": "other", "rank": "",  "update": "0000000000000", "changed": 0},
+    {"id": -10, "category": "Eigene Items",             "type": "dummy",        "custom": "11",     "status": "other", "rank": "",  "update": "0000000000000", "changed": 0}
+
 ]
 
 
--- scripts/itemList.mjs
+++ scripts/itemList.mjs
@@ -1,10 +1,17 @@
 class ItemList {
     itemList = [];
     storageName = "defaultStorageName";
+    categoryList = [];
+    getUrl = "";
+    postUrl = "";
+    //item structure
+    //id, category, type, custom, status, rank, update, changed
 
-    constructor(storageName){
+    constructor(storageName, getUrl, postUrl){
         this.storageName = storageName;
-        console.log("new itemList was created with a storage name of: " + this.storageName);
+        this.getUrl = getUrl;
+        this.postUrl = postUrl;
+        //console.log("new itemList was created with a storage name of: " + this.storageName);
     }
 
     getList() {
@@ -15,17 +22,88 @@ class ItemList {
         this.itemList = itemList;
     }
 
-    getDataFromServer(url){
+    getMaxId(){
+        let maxId = 0;
+        this.itemList.forEach(item => {
+            if (item.id > maxId){
+                maxId = item.id;
+            }
+        })
+        return maxId;
+    }
+
+    getItemByType(type) {
+        let searchedItem;
+        this.itemList.forEach(item => {
+            if (item.type === type){
+                searchedItem = item;
+            }
+        })
+        return searchedItem;
+    }
+
+    updateItem(item){
+        //console.log("updating list with following item: ");
+        //console.log(item);
+        for (let i = 0; i < this.itemList.length; i++){
+            if (item.id === this.itemList[i].id){
+                this.itemList[i] = item;
+            }
+        }
+        localStorage.setItem(this.storageName, JSON.stringify(this.itemList));
+    }
+
+    addItem(item){
+        this.itemList.push(item);
+        localStorage.setItem(this.storageName, JSON.stringify(this.itemList));
+        this.sendDataToServer();
+    }
+
+    increaseRecentRank(){
+        this.itemList.forEach(item => {
+            if (item.status === "recent") {
+                if (parseInt(item.rank) > 5) {
+                    item.rank = "";
+                    item.status = "other";
+                } else {
+                    console.log("increasing rank of item " + item.type);
+                    item.rank = parseInt(item.rank) + 1;
+                }
+            }
+        })
+        //console.log(this.itemList);
+        localStorage.setItem(this.storageName, JSON.stringify(this.itemList));
+    }
+
+    decreaseRecentRank(rank) {
+        //console.log("reducing all ranks above: " + rank);
+        this.itemList.forEach(item => {
+            if (item.status === "recent") {
+                if (parseInt(item.rank) > rank) {
+                    console.log("decreasing rank of item " + item.type);
+                    item.rank = parseInt(item.rank) - 1;
+                }
+            }
+        })
+        //console.log(this.itemList);
+        localStorage.setItem(this.storageName, JSON.stringify(this.itemList));
+    }
+
+    getDataFromServer(){
         //console.log("start loading data from server");
-        //let url = "/api/fresh_list"
-        fetch(url)
-            .then((response) => response.json())
-                .then((data) => this.writeToLocalStorage(data))
-                    .then(() => {
-                        console.log("finished getting all data from server");
-                    })
-                .catch((error) => console.log(error))
-            .catch((error) => console.log(error));
+        return new Promise((resolve, reject) => {
+            fetch(this.getUrl)
+                .then((response) => response.json())
+                    .then((data) => this.writeToLocalStorage(data))
+                        .then(() => {
+                            resolve(
+                                //console.log("finished getting all data from server")
+                            );
+                            reject("something went wrong");
+                        })
+                    .catch((error) => console.log(error))
+                .catch((error) => console.log(error));
+        });
     }
 
     writeToLocalStorage(data){
@@ -33,6 +111,9 @@ class ItemList {
             //"found local storage. Filling this into itemList"
             this.itemList = JSON.parse(localStorage.getItem(this.storageName));
         }
+        //empty list to refill
+        this.categoryList = [];
+
         return new Promise((resolve, reject) => {
             data.forEach(newItem => {
                 //"iterating over all items to push to items"
@@ -44,15 +125,39 @@ class ItemList {
                     if (this.itemList[newItem.id].update < newItem.update){
                         this.itemList[newItem.id] = newItem;
                     }
-                }         
+                }    
+                //extract categories
+                if (newItem.type === "dummy"){
+                    this.categoryList.push(newItem.category);
+                }
             })
+
             //console.log("writing everything to following storage: " + this.storageName);
             localStorage.setItem(this.storageName, JSON.stringify(this.itemList));
+
+            //resolve promise
             resolve(
-                console.log("recieved data was compared and written to the local storage")
+                //console.log("recieved data was compared and written to the local storage")
             );
             reject("something went wrong while comparing or writing to the local storage")
         });
+    }
+
+    sendDataToServer(){
+       /* fetch(this.postUrl, {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.itemList)
+        })
+            .then((resoponse) => {
+                console.log(resoponse);
+            })
+            .catch((error) => {
+                console.log("data was not sent successfully:" + error);
+            })*/
     }
 }
 
